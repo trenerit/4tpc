1fc90923d25a8976484c1751e5ba978bd951eeac
Piotr Kania <piotr@trener-it.pl>
2026-01-08 16:33:12 +0100

    add form handling for firstname input in app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  
-}
+
+  firstname: string = '';
+
+  protected onSubmit(data: any): void {
+    console.log(data.value.firstname);
+    this.firstname = data.value.firstname;
+  }
+ }
